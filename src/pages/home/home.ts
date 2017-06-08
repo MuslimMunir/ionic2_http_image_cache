bbb541ad169af6e04cb5ffa71635862a40848ad0
muslim <muslim028@gmail.com>
2017-06-09 02:57:12 +0500

    Unused import removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 import { CustomHttp } from '../../services/http.service';
 
 import { Movie } from '../../models/movie';
